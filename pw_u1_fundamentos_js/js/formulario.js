2ed03321d107157f2afa29f9d33c8ffae48ff1ac
jisantamariar1 <jisantamariar1@uce.edu.ec>
2025-11-14 19:44:34 -0500

    Maquetacion de formulario

--- pw_u1_fundamentos_js/js/formulario.js
+++ pw_u1_fundamentos_js/js/formulario.js
@@ -1,14 +1,15 @@
-function guardar(){
+function guardar() {
     validarCampos();
 }
-function validarCampos(){
-//validar el contenido del formulario es decir de un input especifico
-    let nombre= document.getElementById("id_nombre").value;
-    let apellido= document.getElementById("id_apellido").value;
-    let fecha= document.getElementById("id_fecha").value;
-    let email= document.getElementById("id_email").value;
-    let password= document.getElementById("id_password").value;
-    if(nombre==="" ){
+function validarCampos() {
+    //validar el contenido del formulario es decir de un input especifico
+    let nombre = document.getElementById("id_nombre").value;
+    let apellido = document.getElementById("id_apellido").value;
+    let fecha = document.getElementById("id_fecha").value;
+    let email = document.getElementById("id_email").value;
+    let password = document.getElementById("id_password").value;
+    limpiarMensajes()
+    if (nombre === "") {
         //solo es un ejemplo
         // el uso de alert no es correcto para enviar mensajes pues es vulnerable
         //alert('Campo incompleto');
@@ -16,58 +17,56 @@ function validarCampos(){
         mostrarAsterisco('id_error_nombre');
         return;
     };
-    if(apellido==="" ){
+
+    if (apellido === "") {
 
         mostrar_mensaje('Apellido necesario');
         mostrarAsterisco('id_error_apellido');
         return;
-    }else{
-        limpiarMensajes();
     };
-    if(fecha==="" ){
+    if (fecha === "") {
 
         mostrar_mensaje('Fecha de nacimiento necesario');
         mostrarAsterisco('id_error_fecha');
         return;
     };
-    if(email===""){
+    if (email === "") {
         mostrar_mensaje('Email necesario');
         mostrarAsterisco('id_error_email');
         return;
 
-    }else if(!validarEmail(email)){
+    } else if (!validarEmail(email)) {
         mostrar_mensaje('Email invalido');
         mostrarAsterisco('id_error_email');
         return;
     };
-    if(password==="" ){
+    if (password === "") {
 
         mostrar_mensaje('Contraseña necesaria');
         mostrarAsterisco('id_error_password');
         return;
     };
-    
+
 }
 
-function mostrar_mensaje(msg){
-    let mensaje =document.getElementById('id_msg_error');
-    mensaje.innerText = msg;
-    mensaje.style.display="block";
-}
-
-function mostrarAsterisco(idElemento){
-    document.getElementById(idElemento).innerText='*';
-}
-function limpiarMensajes(){
+function mostrar_mensaje(msg) {
     let mensaje = document.getElementById('id_msg_error');
-    mensaje.innerText="";
-    mensaje.style.display="none";
-    const errores_asterisco=document.querySelectorAll('.error_asterisco');
-    errores_asterisco.forEach(e=> e.innerText ='');
+    mensaje.innerText = msg;
+    mensaje.style.display = "block";
+}
+
+function mostrarAsterisco(idElemento) {
+    document.getElementById(idElemento).innerText = '*';
+}
+function limpiarMensajes() {
+    let mensaje = document.getElementById('id_msg_error');
+    mensaje.innerText = "";
+    mensaje.style.display = "none";
+    const errores_asterisco = document.querySelectorAll('.error_asterisco');
+    errores_asterisco.forEach(e => e.innerText = '');
 }
 
 function validarEmail(email) {
     const patron = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return patron.test(email);
 }
- 
